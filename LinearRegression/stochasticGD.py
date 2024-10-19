02546f87da598ab1f9e7ae4633aa02242821700e
unknown <jinolshah08@gmail.com>
2024-10-18 23:54:56 -0600

    optimised for linear regression for less repetition

--- LinearRegression/stochasticGD.py
+++ LinearRegression/stochasticGD.py
@@ -1,8 +1,6 @@
 import numpy as np
-import pandas as pd
-import dataset
 import matplotlib.pyplot as plt
-import os
+import getData
 
 def cost_function(data, weight_vector):
     sum = 0.0
@@ -30,15 +28,15 @@ def stochastic_gradient_cost(row, data, weight_vector, learning_rate):
         new_weight_vector[columns] = weight_vector[columns] + learning_rate*((data.iloc[row][-1] - np.dot(weight_vector, data.iloc[row][:-1]))*data.iloc[row][columns])
     return new_weight_vector
 
-def GradientDescent(df_train, df_test, weight_vector, learning_rate, max_iterations):
+def GradientDescent(train_data, test_data, weight_vector, learning_rate, max_iterations):
     cost_iterations_list = []
     weight_vector_list = []
     iteration = 1
     while True:
         if iteration<max_iterations:
-            for row in range(df_train.shape[0]):
-                new_weight_vector = stochastic_gradient_cost(row, df_train, weight_vector, learning_rate)
-            cost_iterations_list.append(cost_function(df_train, weight_vector))
+            for row in range(train_data.shape[0]):
+                new_weight_vector = stochastic_gradient_cost(row, train_data, weight_vector, learning_rate)
+            cost_iterations_list.append(cost_function(train_data, weight_vector))
             if np.linalg.norm(new_weight_vector - weight_vector) < 0.000001:
                 break
             else:
@@ -47,7 +45,7 @@ def GradientDescent(df_train, df_test, weight_vector, learning_rate, max_iterati
                 weight_vector_list.append(weight_vector)
         else:
             learning_rate /= 4
-            weight_vector = np.asarray([0.0] * (df_train.shape[1] - 1))
+            weight_vector = np.asarray([0.0] * (train_data.shape[1] - 1))
             weight_vector_list = list()
             cost_iterations_list = list()
             weight_vector_list.append(weight_vector)
@@ -55,7 +53,7 @@ def GradientDescent(df_train, df_test, weight_vector, learning_rate, max_iterati
         
     print("The final weight vector is: ", weight_vector_list[-1])
     print("The learning rate is: ", learning_rate)
-    print("Cost of test data set: ", cost_function(df_test, weight_vector_list[-1]))
+    print("Cost of test data set: ", cost_function(test_data, weight_vector_list[-1]))
     plt.plot(range(len(cost_iterations_list)), cost_iterations_list)
     plt.xlabel('Iterations')
     plt.ylabel('Cost Function')
@@ -63,15 +61,8 @@ def GradientDescent(df_train, df_test, weight_vector, learning_rate, max_iterati
     plt.show()
 
 if __name__ == "__main__":
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    bank_train_data_file = os.path.join(script_dir, "concrete/train.csv")
-    bank_test_data_file = os.path.join(script_dir, "concrete/train.csv")
+    train_data, test_data = getData.getData()
 
-    df_train = pd.read_csv(bank_train_data_file)
-    df_train.columns = dataset.concrete_columns
-    df_test = pd.read_csv(bank_test_data_file)
-    df_test.columns = dataset.concrete_columns
+    weight_vector = np.asarray([0.0] * (train_data.shape[1] - 1))
 
-    weight_vector = np.asarray([0.0] * (df_train.shape[1] - 1))
-
-    GradientDescent(df_train, df_test, weight_vector,  1, 5000)
+    GradientDescent(train_data, test_data, weight_vector,  1, 5000)
